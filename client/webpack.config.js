da0e82bbe2a6d6d7a47c4ae02b9b11bdc2a1e4bf
Miro Spönemann <miro.spoenemann@typefox.io>
2017-07-12 11:43:49 +0200

    Added uglify-loader to reduce code size

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,44 +14,56 @@ const monacoEditorPath = 'node_modules/monaco-editor-core/dev/vs';
 const bootstrapDistPath = 'node_modules/bootstrap/dist';
 const sprottyCssPath = 'node_modules/sprotty/css';
 
-module.exports = {
-    entry: {
-        editor: path.resolve(buildRoot, 'editor-main.js'),
-        models: path.resolve(buildRoot, 'models-main.js')
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: appRoot
-    },
-    module: {
-        noParse: /vscode-languageserver-types/
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            'vs': path.resolve(__dirname, monacoEditorPath)
-        }
-    },
-    devtool: 'source-map',
-    target: 'web',
-    node: {
-        fs: 'empty',
-        child_process: 'empty',
-        net: 'empty',
-        crypto: 'empty'
-    },
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: monacoEditorPath,
-            to: 'vs'
-        }]),
-        new CopyWebpackPlugin([{
-            from: bootstrapDistPath,
-            to: 'bootstrap'
-        }]),
-        new CopyWebpackPlugin([{
-            from: sprottyCssPath,
-            to: 'sprotty'
-        }])
-    ]
+module.exports = function(env) {
+    if (!env) {
+        env = {}
+    }
+    return {
+        entry: {
+            editor: path.resolve(buildRoot, 'editor-main.js'),
+            models: path.resolve(buildRoot, 'models-main.js')
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: appRoot
+        },
+        module: {
+            noParse: /vscode-languageserver-types/,
+            loaders: env.uglify ? [
+                {
+                    test: /.*\.js$/,
+                    exclude: /vscode-base-languageclient/,
+                    loader: 'uglify-loader'
+                }
+            ] : []
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                'vs': path.resolve(__dirname, monacoEditorPath)
+            }
+        },
+        devtool: 'source-map',
+        target: 'web',
+        node: {
+            fs: 'empty',
+            child_process: 'empty',
+            net: 'empty',
+            crypto: 'empty'
+        },
+        plugins: [
+            new CopyWebpackPlugin([{
+                from: monacoEditorPath,
+                to: 'vs'
+            }]),
+            new CopyWebpackPlugin([{
+                from: bootstrapDistPath,
+                to: 'bootstrap'
+            }]),
+            new CopyWebpackPlugin([{
+                from: sprottyCssPath,
+                to: 'sprotty'
+            }])
+        ]
+    }
 }
