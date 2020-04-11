d8a5a4b04e04e9efb8902c0d40060a794390fdf7
Ulf Rüegg <uruurumail@gmail.com>
2020-04-11 22:10:39 +0200

    client.elkgraph: added version select box and custom sprotty ChangeLayoutVersionAction

--- client/src/elkgraph/language-diagram-server.ts
+++ client/src/elkgraph/language-diagram-server.ts
@@ -6,13 +6,25 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 import { IConnection } from "monaco-languageclient/lib/connection";
-import { DiagramServer, ActionMessage } from "sprotty";
+import { DiagramServer, ActionMessage, ActionHandlerRegistry, Action } from "sprotty";
 
 const DIAGRAM_ENDPOINT_NOTIFICATION = 'diagram/accept';
 const DID_CLOSE_NOTIFICATION = 'diagram/didClose';
 
-export default class LanguageDiagramServer extends DiagramServer {
+export class ChangeLayoutVersionAction implements Action {
+    static readonly KIND = 'versionChange';
+    readonly kind = ChangeLayoutVersionAction.KIND;
+
+    constructor(public readonly version?: string) {}
+}
+
+export class LanguageDiagramServer extends DiagramServer {
     protected connection?: IConnection;
+
+    initialize(registry: ActionHandlerRegistry): void {
+        super.initialize(registry);
+        registry.register(ChangeLayoutVersionAction.KIND, this);
+    }
 
     listen(connection: IConnection) {
         connection.onNotification(DIAGRAM_ENDPOINT_NOTIFICATION, (message: ActionMessage) => {
