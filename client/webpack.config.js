0ee850f9cc4b215b5bc11f314052f9e122360c62
Max Kasperowski <Eddykasp@users.noreply.github.com>
2024-05-22 12:11:43 +0200

    Update for elk 0.9.0 (#70)
    * Add new elkjs version 0.9.0
    
    * remove unnecessary whitespace
    
    * add elkjs 0.9.0 to yarn.lock
    
    * update lockfile with correct versions
    
    * correct elk webworker reference
    
    * update to use current 0.9.3 version of elkjs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,7 @@ module.exports = async function (env) {
     const elkWorkerPath7 = 'node_modules/elkjs-7/lib/elk-worker.min.js';
     const elkWorkerPath7_1 = 'node_modules/elkjs-7-1/lib/elk-worker.min.js';
     const elkWorkerPath8 = 'node_modules/elkjs-8/lib/elk-worker.min.js';
+    const elkWorkerPath9 = 'node_modules/elkjs-9/lib/elk-worker.min.js';
     const elkWorkerPathLatest = 'node_modules/elkjs-latest/lib/elk-worker.min.js';
     const elkWorkerPathNext = 'node_modules/elkjs-next/lib/elk-worker.min.js';
     const currentGitCommit = childProcess.execSync('git rev-parse --short HEAD').toString().trim();
@@ -202,6 +203,10 @@ module.exports = async function (env) {
             new CopyWebpackPlugin([{
                 from: elkWorkerPath8,
                 to: 'elk-8'
+            }]),
+            new CopyWebpackPlugin([{
+                from: elkWorkerPath9,
+                to: 'elk-9'
             }]),
             new CopyWebpackPlugin([{
                 from: elkWorkerPathLatest,
