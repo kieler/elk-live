bb94ad950d5b3ad04dbadd30bf031657dd1a2b13
Max Kasperowski <mka@informatik.uni-kiel.de>
2025-03-14 09:21:14 +0100

    add licence headers and update package.json version

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Kiel University and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
 import { setupDarkMode } from "./common/dark-mode";
 
 setupDarkMode();
