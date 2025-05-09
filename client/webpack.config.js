8126e766e2837fd739a66c707fb5092603b74b23
Max Kasperowski <mka@informatik.uni-kiel.de>
2025-05-09 14:56:10 +0200

    update maven search links to new url

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = async function (env) {
     const javaElkVersions = [ 'snapshot' ]; // latest snapshot/nightly at the time of building 
     // Query released ELK versions using maven's REST API
     try {
-        const response = await fetch("https://search.maven.org/solrsearch/select?q=g:%22org.eclipse.elk%22+AND+a:%22org.eclipse.elk.core%22&core=gav&wt=json")
+        const response = await fetch("https://central.sonatype.com/solrsearch/select?q=g:%22org.eclipse.elk%22+AND+a:%22org.eclipse.elk.core%22&core=gav&wt=json")
                                     .then(res => res.json());
         response.response.docs.forEach(doc => {
             javaElkVersions.push(doc.v);
